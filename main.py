08895f2810569b399d6d52b319b22cb0e3eb66eb
Zlofey <tatarovtk@gmail.com>
2023-10-24 19:07:08 +0300

    add toml

--- main.py
+++ main.py
@@ -13,12 +13,16 @@ import logging
 import click
 import psutil as psutil
 import filecmp
+import toml
 
 
 def main():
+    with open("sample.toml", "r") as f:
+        data = toml.load(f)
+        
     print("job is done")
     print("\n*****Done!!!*****")
-
+    print(data["foo"])
 
 if __name__ == "__main__":
     main()
